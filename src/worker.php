334048f5e44f9923797d11f4df89e11670112dec
Дмитрий Гладких <d.gladkih@opticcenter.ru>
2025-07-06 19:35:03 +0300

    Добавлены уведомления в телеграм

--- src/worker.php
+++ src/worker.php
@@ -2,6 +2,7 @@
 
 use App\Handlers\IWorker;
 use App\Queue;
+use GuzzleHttp\Client;
 
 set_time_limit(0);
 
@@ -36,6 +37,15 @@ try {
 } catch (Throwable $t) {
     $queue->rollback($task_id);
     echo $t->getMessage() . "\n";
+
+    $client = new Client(['base_uri' => 'https://api.telegram.org']);
+    $client->post('/bot' . $_ENV['TG_BOT_TOKEN'] . '/sendMessage', [
+        'form_params' => [
+            'chat_id' => $_ENV['TG_CHAT_ID'],
+            'text' => 'Ошибка YoutubeToVK: ' . $t->getMessage(),
+            'parse_mode' => 'MarkdownV2'
+        ],
+    ]);
 } finally {
     $queue->releaseLock();
 }
